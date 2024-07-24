73a2b79acc532a30a405b6a5bbba393bf724e099
孙成龙 <18715672252@163.com>
2024-07-24 17:28:22 +0800

    监听网络连接

--- src/renderer/src/pages/home/home.tsx
+++ src/renderer/src/pages/home/home.tsx
@@ -4,6 +4,7 @@ import pin from '../../img/pin.png'
 import close from '../../img/close.png'
 import shotSel from '../../img/shotSelWhite.png'
 import rondom from '../../img/rondom.png'
+import rondomoffline from '../../img/rondomoffline.png'
 import screenrecording from '../../img/screenrecording.png'
 import './home.css'
 import ModalCust from '@renderer/components/modal'
@@ -13,11 +14,13 @@ const Home: FC = (): JSX.Element => {
   const [imgH, setImgH] = React.useState(10)
   const [isModalOpen, setIsModalOpen] = React.useState(false)
   const [loading, setLoding] = React.useState(false)
+  const [netStatus, setNetStatus] = React.useState(true)
   const getpp = (): void => {
     // router('/setting')
     window.api.sendIpcMain('desktopCapturer')
   }
   const rondomImg = (): void => {
+    if (!netStatus) return
     setIsModalOpen(true)
   }
   const generateImg = async (): Promise<unknown> => {
@@ -28,6 +31,15 @@ const Home: FC = (): JSX.Element => {
     setLoding(false)
     return 1
   }
+  React.useEffect(() => {
+    window.addEventListener('online', function () {
+      setNetStatus(true)
+    })
+
+    window.addEventListener('offline', function () {
+      setNetStatus(false)
+    })
+  }, [])
   return (
     <div className="home">
       <ModalCust
@@ -51,8 +63,8 @@ const Home: FC = (): JSX.Element => {
         <div onClick={getpp} title="截图">
           <img src={shotSel} alt="" />
         </div>
-        <div title="随机图片" onClick={rondomImg}>
-          <img src={rondom} alt="" />
+        <div title={netStatus ? '随机图片' : '网络已断开'} onClick={rondomImg}>
+          {netStatus ? <img src={rondom} alt="" /> : <img src={rondomoffline} alt="" />}
         </div>
         <div title="屏幕录制" onClick={rondomImg}>
           <img src={screenrecording} alt="" />
